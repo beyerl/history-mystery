d1aedfc3a987313bebdbeb973ed7c9d78a635a7d
Lorenz Beyer <lorenzbeyer@posteo.de>
2025-05-07 23:34:18 +0200

    adjust main layout

--- WebFrontend/wwwroot/pages/start-page.js
+++ WebFrontend/wwwroot/pages/start-page.js
@@ -1,10 +1,12 @@
 class StartPage extends HTMLElement {
     connectedCallback() {
         this.innerHTML = `
-            <h1>History Mystery</h1>
-            <div style="text-align: center; margin-top: 20%; display: flex; flex-direction: column; align-items: stretch;">
-                <button id="start-game-button" style="padding: 10px 20px; font-size: 16px;">Start Game</button>
-                <button id="join-game-button" style="padding: 10px 20px; font-size: 16px;">Join Game</button>
+            <div style="height: 100vh; display: flex; flex-direction: column; justify-content: center; align-items: center;">
+                <h1 style="color: white; text-shadow: 2px 2px 4px rgba(0, 0, 0, 0.7); margin-bottom: 65vh;">Herstory Mystery</h1>
+                <div style="text-align: center; margin-top: 20px; display: flex; flex-direction: column; align-items: stretch;">
+                    <button id="start-game-button" style="padding: 10px 20px; font-size: 16px; background-color: var(--color-mint); color: var(--color-brown); border: none; border-radius: 5px;">Start Game</button>
+                    <button id="join-game-button" style="padding: 10px 20px; font-size: 16px; background-color: var(--color-yellow); color: var(--color-brown); border: none; border-radius: 5px; margin-top: 10px;">Join Game</button>
+                </div>
             </div>
         `;
 
